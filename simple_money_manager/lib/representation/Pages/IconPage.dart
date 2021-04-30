872abc7fe8bfd4e5263e19b402fe97d79e71d3bd
Joe Nguyen <47797190+joenguyen0109@users.noreply.github.com>
2021-04-30 23:53:50 +1000

    Update IconPage file

--- simple_money_manager/lib/representation/Pages/IconPage.dart
+++ simple_money_manager/lib/representation/Pages/IconPage.dart
@@ -19,45 +19,26 @@ class IconPage extends StatelessWidget {
             icon: Icon(Icons.arrow_back),
             onPressed: () => Get.back(),
           ),
-          actions: [
-            IconButton(
-                icon: Icon(Icons.arrow_forward_outlined),
-                onPressed: () {
-                  context.read<MainBloc>().add(
-                        AddTransactionToDataBaseEvent(),
-                      );
-                  context.read<DropdownCubit>().onChanged(null);
-                }),
-          ],
         ),
       ),
       body: Container(
           padding: EdgeInsets.all(16.0),
-          child: DragSelectGridView(
-            itemCount: path.length,
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 3,
-              crossAxisSpacing: 8,
-              mainAxisSpacing: 8,
-            ),
-            itemBuilder: (_, index, isSelected) {
-              print('run');
-              return Container(
-                child: Image.asset(path[index]),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(5.0),
-                  boxShadow: isSelected
-                      ? [
-                          BoxShadow(
-                            color: Colors.grey,
-                            offset: Offset(0.0, 1.0), //(x,y)
-                            blurRadius: 6.0,
-                          ),
-                        ]
-                      : [],
-                ),
+          child: GridView.count(
+            scrollDirection: Axis.vertical,
+            crossAxisCount: 3,
+            children: path.map<InkWell>((value) {
+              return InkWell(
+                // highlightColor: null,
+                onTap: () {
+                  context.read<MainBloc>().add(
+                        AddTransactionToDataBaseEvent(),
+                      );
+                  context.read<DropdownCubit>().onChanged(null);
+                },
+
+                child: Image.asset(value),
               );
-            },
+            }).toList(),
           )),
     );
   }
